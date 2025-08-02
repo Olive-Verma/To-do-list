1ea313d3a5886868f3ebc54e967dde238ba112c1
Olive-Verma <151298olive@gmail.com>
2025-08-02 22:57:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,220 @@
+// 1.Ways to print in javascript
+//console.log('Hello World');
+//  alert("me");
+// document.write("this is document write"); 
+
+//2.Javascript console.API
+//console.log("Hello World", 4 + 6, "Another log");
+//console.warn("this is warn");
+//console.error("this is an error"); 
+
+// 3. Javascript variables
+// what are Variables? -containers to store data values
+
+/*
+multi 
+line 
+comments
+*/
+
+/*
+var number1 = 10;
+var number2 = 20;
+//console.log(number1 + number2);
+
+// 4.Datatypes in javascript
+// Number
+ var num1 = 23;
+ var num2 = 24.3
+ 
+
+ //string
+ var str1 = "This is a string";
+ var str2 ="This is also a  string";
+
+
+ //Object
+ var marks = {
+ravi:31,
+rajan:32,
+patel:33.55,
+ }
+ //console.log('abcd')
+ //console.log(marks);
+
+ //Boleans
+ var a = true;
+ var b = false;
+ //console.log(a, b);
+
+var und = undefined;
+ var n = unddefined;
+ //console.log(n);
+
+*/
+
+//t a very high level,  there are two types of data types in javascript
+//1. Primitive data types: undefined, null, number ,string, boolean,symbol
+//Reference data types: Arrays and objects 
+
+
+
+
+var arr = [1, 2, "bablu", 4, 5]
+//console.log(arr);
+
+
+//Opertors in javascript
+//Arithmetics Operators
+
+var a = 10;
+var b = 100;
+
+//console.log("the value of a + b is", a + b)
+//console.log("the value of a - b is", a - b);
+//console.log("the value of a * b is", a * b);
+//console.log("the value of a / b is", a / b);
+
+
+//Assignment Operators
+var z = b;
+z += 2;
+z -= 2; //z = -2;
+z *= 2;
+z /= 2;
+//console.log(z);
+
+// Comparison Operators
+/*
+var x = 34;
+var y = 56;
+console.log(x === y) 
+console.log(x => y)
+console.log(x <= y)
+console.log(x < y)
+console.log(x > y)
+*/
+
+/*
+logical and
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
+
+/*
+ //logical or
+console.log(true || true)
+console.log(true || false)
+console.log(false|| true)
+console.log(false || false)
+*/
+
+//logical not
+//console.log(!false)
+//console.log(!true)
+
+//Function in javascript
+//Dry = do not repeat yourself
+/*function avg(a, b, d){
+c =(a + b )/2
+return c;
+}
+c1 = (4, 8);
+c2 = (8, 6);
+*/
+//console.log(c1, c2);
+//conditional in javascript
+// if statement
+/*
+var age = 41;
+if (age >25){
+  console.log("you can drink water")
+}
+//else statement
+//var age = 31;
+
+
+/*
+var age = 41;
+if(age >18 ){
+  console.log("you can drink rasana water");
+}
+if (age > 5){
+console.log("you cannot drink rasana water");
+}
+// if-else statement
+var age = 25;
+if (age > 45 ){
+console.log("Verma can eat food")
+}
+else (age > 10){
+  console.log("verma cannot eat food")
+}
+  */
+
+
+/*
+else{
+  console.log("you cannot drink rasana water")
+}
+
+
+/*
+var arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr)
+for (var i = 0; i <arr.length;i++)
+{
+  console.log(arr[i])
+}
+
+
+//if-else Ladder
+if(age >32){
+  console.log("you are not a kid");
+}
+else 
+*/
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(arr);
+//
+
+//arr.forEach(function (element){
+//console.log(element)
+//})
+
+//const ac = 0;
+//ac++;
+//ac = ac + 1;
+
+//let j = 0;
+//while (j < arr.length) {
+  //console.log(arr[j])
+  //j++;
+//} 
+
+  let myArr = ["fan", " camera", 34, null , true];
+ //
+   
+
+//Array Methods
+console.log(myArr.length);
+//myArr.pop();
+myArr.push("harry")
+myArr.unshift("harry")
+//console.log(myArr.unfinished("Harry")
+//console.log(newLen);
+console.log(myArr)
+
+/*
+
+console.log(myArr.length);
+/*
+ do{
+   console.log(arr[j]);
+   j++;
+  } while (j< arr.length);
+  */
+//let myArr = ["fan" , "camera" ,]
+
